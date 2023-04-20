b72581a2f92cca485128cb2855d57de47aa5cc22
sebastiaolsousa <sebastiao.luzsousa@gmail.com>
2023-04-20 17:39:41 -0300

    almost complet

---
+++ script.js
@@ -0,0 +1,22 @@
+var res = []
+
+function opponentplay() {
+    var opplay = ["rock", "paper", "scissor"]
+    var randomplay = Math.floor(Math.random() * opplay.length);
+    var randomres = opplay[randomplay]
+    return randomres
+}
+
+function play(choice){
+    switch(choice) {
+        case 'rock':
+
+            break
+        case 'paper':
+
+            break
+        case 'scissors':
+
+            break
+    }
+}
